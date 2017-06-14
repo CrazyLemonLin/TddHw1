7b33a984adde520e9f2dca96a36fbb1349cea7e0
CrazyLemonLin <lemonlinchju@hotmail.com>
2017-06-14 20:57:04 +0800

    加上測試資料

---
+++ DynamicCalculatorTest/DynamicCalculatorTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DynamicCalculatorTest
+{
+    [TestClass]
+    public class DynamicCalculatorTest
+    {
+        private  List<dynamic> _orders;
+        [ClassInitialize]
+        public void SetUp()
+        {
+            _orders = new List<dynamic> {
+                new
+                {
+                    Id =1,Cost=1,Revenue=11,SellPrice=21
+                },
+                 new
+                {
+                    Id =2,Cost=2,Revenue=12,SellPrice=22
+                },
+                 new
+                {
+                    Id =3,Cost=3,Revenue=13,SellPrice=23
+                },
+                  new
+                {
+                    Id =4,Cost=4,Revenue=14,SellPrice=24
+                },
+                   new
+                {
+                    Id =5,Cost=5,Revenue=15,SellPrice=25
+                },
+                    new
+                {
+                    Id =6,Cost=6,Revenue=16,SellPrice=26
+                },
+                     new
+                {
+                    Id =7,Cost=7,Revenue=17,SellPrice=27
+                },
+                      new
+                {
+                    Id =8,Cost=8,Revenue=18,SellPrice=28
+                },
+                        new
+                {
+                    Id =9,Cost=9,Revenue=19,SellPrice=29
+                },
+                        new
+                {
+                    Id =10,Cost=10,Revenue=20,SellPrice=30
+                },
+                        new
+                {
+                    Id =11,Cost=11,Revenue=21,SellPrice=31
+                }
+            };
+
+        }
+    }
+}
